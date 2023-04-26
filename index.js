3a77201b47a0fb5fb7f43ae09b67c50296f053de
rbgonzal <66926860+rbgonzal@users.noreply.github.com>
2023-04-26 11:33:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const content_body = document.getElementById('content_body')
+const slatepayment = 'slatepayment.html'
 function openTab1() {
     
     content_body.style.display = "block";
@@ -12,4 +13,18 @@ function openProjects(){
     fetch('projects.html').then((response) => {
         return response.text()
     }).then((html) => content_body.innerHTML = html)
+}
+
+function openModal(proj){
+    proj = slatepayment
+    const modal_container = document.getElementById('modal_container')
+    const modal_content = document.getElementById('modal_content')
+    modal_container.style.display="block"
+    fetch(proj).then((response) => {
+        return response.text()
+    }).then((html) => modal_content.innerHTML = html)
+}
+
+function closeModal(){
+    document.getElementById("modal_container").style.display="none"
 }
